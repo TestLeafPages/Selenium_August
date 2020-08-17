18f6584e3912e0ae780d4ec773ce2644d2beaef8
TestLeafPages <sarath.muthuswamy@testleaf.com>
2020-08-17 09:16:09 +0530

    Week 2 Code Update

--- week2/day1/LaunchChromeBrowser.java
+++ week2/day1/LaunchChromeBrowser.java
@@ -1,7 +1,10 @@
 package week2.day1;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -13,6 +16,9 @@ public static void main(String[] args) {
 	FirefoxDriver driver = new FirefoxDriver();
 	// Load the url in it
 	driver.get("http://leaftaps.com/opentaps/");
+	
+	// Implicit wait
+	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	
 	// To maximise the browser
 	driver.manage().window().maximize();
@@ -26,6 +32,35 @@ public static void main(String[] args) {
 	driver.findElementByClassName("decorativeSubmit").click();
 	
 	driver.findElementByLinkText("CRM/SFA").click();
+	
+	driver.findElementByLinkText("Leads").click();
+	
+	driver.findElementByLinkText("Create Lead").click();
+	
+	//driver.findElementById("createLeadForm_companyName").sendKeys("Testleaf");
+	
+	// Type - I => Simple drop down handling
+	
+	// driver.findElementById("createLeadForm_dataSourceId").sendKeys("Partner");
+	
+	// Type - II => Handling using Select class
+	
+	WebElement elementDropDown = driver.findElementById("createLeadForm_dataSourceId");
+	
+	Select obj = new Select(elementDropDown);
+	
+	// Select using visible text
+	// obj.selectByVisibleText("Direct Mail");
+	
+	// Select using Index
+	// obj.selectByIndex(4);
+	
+	// Select using value
+	obj.selectByValue("LEAD_WEBSITE");
+	
+	
+	
+	
 	
 	
 	
