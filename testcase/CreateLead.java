2ebf2eb69341906cd936bd782a0e08cd9dc344f7
TestLeafPages <sarath.muthuswamy@testleaf.com>
2020-09-05 20:40:57 +0530

    Add files via upload

---
+++ testcase/CreateLead.java
@@ -0,0 +1,27 @@
+package testcase;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CreateLead extends BaseClass {
+
+	@Test
+	public void runCreateLead() {
+		driver.findElementByLinkText("Create Lead").click();
+		driver.findElementById("createLeadForm_companyName").sendKeys("TestLeaf");
+		driver.findElementById("createLeadForm_firstName").sendKeys("Gopi");
+		driver.findElementById("createLeadForm_lastName").sendKeys("J");
+		driver.findElementByName("submitButton").click();
+		
+}
+}
+
+
+
+
+
+
