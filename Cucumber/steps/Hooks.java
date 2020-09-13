0768382cd600f441ff28cf9deaa2e013a6b620cf
TestLeafPages <sarath.muthuswamy@testleaf.com>
2020-09-13 19:05:12 +0530

    Add files via upload

--- Cucumber/steps/Hooks.java
+++ Cucumber/steps/Hooks.java
@@ -2,6 +2,7 @@ package steps;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -9,17 +10,25 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Hooks extends BaseClass {
 
 	@Before
-	public void preCondition() {
+	public void preCondition(Scenario sc) {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("http://leaftaps.com/opentaps/control/main");
+		
+		System.out.println(sc.getName());
+		
+		System.out.println(sc.getId());
+		
+		
 
 	}
 
 	@After
-	public void postCondition() {
+	public void postCondition(Scenario sc) {
 		driver.close();
+		
+		System.out.println(sc.getStatus());
 
 	}
 
